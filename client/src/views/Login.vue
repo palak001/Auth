eafd7d6aa618acbbded4ea7a024fb4ee05f2e32b
Palak <palak7372@gmail.com>
2020-02-28 11:35:04 +0530

    Login form added

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,31 @@
 <template>
+  <section>
     <h1>Login Page</h1>
+    <form>
+       <div class="form-group">
+          <label for="username">Username</label>
+          <input
+           type="text"
+           class="form-control"
+           id="username"
+           aria-describedby="usernameHelp">
+           <h5 id="usernameHelp" class="form-text text-muted">
+            Enter Username.
+           </h5>
+       </div>
+       <div class="form-group">
+          <label for="password">Password</label>
+          <input
+           type="password"
+           class="form-control"
+           id="password"
+           aria-describedby="passwordHelp">
+           <h5 id="passwordHelp" class="form-text text-muted">
+            Enter Password.
+           </h5>
+       </div>
+    </form>
+  </section>
 </template>
 
 <script>
